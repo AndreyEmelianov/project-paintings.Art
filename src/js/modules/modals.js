e0d3b29eea4e5b536f07018cc2db7ab9974a438a
Andrey Emelianov <621077@mail.ru>
2023-04-12 17:14:05 +0300

    добавил в проект модуль modals из предыдущего проекта отвечающий за взаимодействие с модальными окнами, подставил селекторы из данного проекта, изменил логику появления модального окна с помощью setTimeout

---
+++ src/js/modules/modals.js
@@ -0,0 +1,98 @@
+const modals = () => {
+	function bindModal(triggerSelector, modalSelector, closeSelector, destroy = false) {
+		const trigger = document.querySelectorAll(triggerSelector),
+			modal = document.querySelector(modalSelector),
+			close = document.querySelector(closeSelector),
+			windowsModal = document.querySelectorAll('[data-modal]'),
+			scroll = calcScroll();
+
+		trigger.forEach((triggerItem) => {
+			triggerItem.addEventListener('click', (e) => {
+				if (e.target) {
+					e.preventDefault();
+				}
+
+				if (destroy) {
+					triggerItem.remove();
+				}
+
+				windowsModal.forEach((windModal) => {
+					windModal.style.display = 'none';
+				});
+
+				modal.style.display = 'block';
+				document.body.style.overflow = 'hidden';
+				document.body.style.marginRight = `${scroll}px`;
+			});
+		});
+
+		close.addEventListener('click', () => {
+			windowsModal.forEach((windModal) => {
+				windModal.style.display = 'none';
+			});
+
+			modal.style.display = 'none';
+			document.body.style.overflow = '';
+			document.body.style.marginRight = `0px`;
+		});
+
+		modal.addEventListener('click', (e) => {
+			if (e.target === modal) {
+				windowsModal.forEach((windModal) => {
+					windModal.style.display = 'none';
+				});
+
+				modal.style.display = 'none';
+				document.body.style.overflow = '';
+				document.body.style.marginRight = `0px`;
+			}
+		});
+	}
+
+	function showModalByTime(selector, time) {
+		setTimeout(() => {
+			let display;
+
+			document.querySelectorAll('[data-modal]').forEach((modalItem) => {
+				if (getComputedStyle(modalItem).display !== 'none') {
+					display = 'block';
+				}
+			});
+
+			if (!display) {
+				document.querySelector(selector).style.display = 'block';
+				document.body.style.overflow = 'hidden';
+
+				let scroll = calcScroll();
+				document.body.style.marginRight = `${scroll}px`;
+			}
+		}, time);
+	}
+
+	function calcScroll() {
+		let div = document.createElement('div');
+
+		div.style.width = '50px';
+		div.style.height = '50px';
+		div.style.overflowY = 'scroll';
+		div.style.visibility = 'hidden';
+
+		document.body.appendChild(div);
+
+		let scrollWidth = div.offsetWidth - div.clientWidth;
+		div.remove();
+
+		return scrollWidth;
+	}
+
+	bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+	bindModal(
+		'.button-consultation',
+		'.popup-consultation',
+		'.popup-consultation .popup-close'
+	);
+
+	showModalByTime('.popup-consultation', 5000);
+};
+
+export default modals;
